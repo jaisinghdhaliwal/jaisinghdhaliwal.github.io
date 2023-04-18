f8584dbaaea7bd0ccf9e7098f90c6809b33cc337
Jai <46607251+jaisinghdhaliwal@users.noreply.github.com>
2023-04-19 00:17:00 +0100

    pok

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-window.onload = function() {
-    let chk = document.getElementById('Toggle');
-    let menuLinks = document.querySelectorAll('.menu a');
-    menuLinks.forEach(function(item) {
-        item.addEventListener('click', function() {chk.checked=false;});
-    });
-}
+window.onload=function(){let chk=document.getElementById("Toggle");let menuLinks=document.querySelectorAll(".menu a");menuLinks.forEach(function(a){a.addEventListener("click",function(){chk.checked=false})})};
