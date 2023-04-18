806c720f88926aa68e062ef09968e0b7ffe6122b
Jai <46607251+jaisinghdhaliwal@users.noreply.github.com>
2023-04-18 23:51:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-// Run once everything is loaded
 window.onload = function() {
-    // Get the checkbox
     let chk = document.getElementById('Toggle');
-    // Get all menu links
     let menuLinks = document.querySelectorAll('.menu a');
-    // Loop on links
     menuLinks.forEach(function(item) {
-        // Add event listener to each links
-        item.addEventListener('click', function() {
-            // When link is clicked, uncheck the checkbox to hide menu
-            chk.checked=false;
-        });
+        item.addEventListener('click', function() {chk.checked=false;});
     });
 }
