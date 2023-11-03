b307f4afdb8fe72fadbd2d8f58a8b866f5cbc6af
Jai <46607251+jaisinghdhaliwal@users.noreply.github.com>
2023-11-03 10:12:09 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    let chk = document.getElementById('Toggle');
+    let menuLinks = document.querySelectorAll('.menu a');
+    menuLinks.forEach(function(item) {
+        item.addEventListener('click', function() {chk.checked=false;});
+    });
+}
