48b15f248626507b533657382ecdd45ba7ede360
Jai <46607251+jaisinghdhaliwal@users.noreply.github.com>
2023-12-12 14:27:20 +0000

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    let chk = document.getElementById('Toggle');
-    let menuLinks = document.querySelectorAll('.menu a');
-    menuLinks.forEach(function(item) {
-        item.addEventListener('click', function() {chk.checked=false;});
-    });
-}
-document.addEventListener('touchstart', onTouchStart, {passive: true});
